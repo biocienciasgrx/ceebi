37c4dadd1b681903343a52de4e3e665d51acd484
Hipy Cas <hipycas@gmail.com>
2021-12-08 13:38:34 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.hipycas.ceebi',
+  appName: 'CEEBI',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
