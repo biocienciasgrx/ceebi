9041006887b71946f891e56985d481458d690308
Hipy Cas <hipycas@gmail.com>
2022-02-05 18:26:36 +0100

    Setup pull notifications

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,23 @@
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet, isPlatform } from "@ionic/vue";
-import { onMounted } from "vue";
+import { onMounted, toHandlerKey } from "vue";
 
 import { LocalNotifications } from "@capacitor/local-notifications";
-import { PushNotifications } from "@capacitor/push-notifications";
+import {
+  PushNotifications,
+  Token,
+  PushNotificationSchema,
+  ActionPerformed,
+} from "@capacitor/push-notifications";
 import { SplashScreen } from "@capacitor/splash-screen";
 import { Filesystem, Directory } from "@capacitor/filesystem";
 
 import { IMAGES_DIRECTORY } from "@/vars";
+
+PushNotifications.addListener("registration", async (token: Token) => {
+  console.log("===== FIREBASE TOKEN: " + token.value + " =====");
+}).catch((e) => console.warn(">>>> FIREBASE REGISTRATION ERROR: " + e));
 
 Filesystem.mkdir({
   path: IMAGES_DIRECTORY,
@@ -31,6 +40,26 @@ if (isPlatform("capacitor"))
     console.log("App:26 push permissions: ", res.receive);
     if (res.receive === "granted") PushNotifications.register();
   });
+
+PushNotifications.addListener(
+  "pushNotificationReceived",
+  async (notification: PushNotificationSchema) => {
+    console.log("PUSH >>>> Push received: " + JSON.stringify(notification));
+  }
+);
+
+PushNotifications.addListener(
+  "pushNotificationActionPerformed",
+  async (notification: ActionPerformed) => {
+    const data = notification.notification.data;
+    console.log(
+      "PUSH >>>> Action performed: " + JSON.stringify(notification.notification)
+    );
+    if (data) {
+      console.info("PUSH >>> data " + JSON.stringify(data));
+    }
+  }
+);
 
 onMounted(SplashScreen.hide);
 </script>
