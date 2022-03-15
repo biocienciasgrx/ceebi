8ffe5766a7ea505ef82f5a10ddefcc77e5a3954d
Hipy Cas <hipycas@gmail.com>
2022-03-15 16:45:01 +0100

    Add module declaration for PNG imports
    Adds a `declare module "*.png"` to the shims-vue type file so TS does
    not raise an error when trying to import a PNG file. Also reformatted
    the file.

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,10 @@
-declare module '*.vue' {
-  import { defineComponent } from 'vue'
-  const component: ReturnType<typeof defineComponent>
-  export default component
+declare module "*.vue" {
+  import { defineComponent } from "vue";
+  const component: ReturnType<typeof defineComponent>;
+  export default component;
+}
+
+declare module "*.png" {
+  const text: string;
+  export default text;
 }
